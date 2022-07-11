92c40bd5170af0fd21739b8c4bf7232bded241e2
chinh1506 <chinhhoang115@gmail.com>
2022-07-11 11:20:28 +0700

    create project

---
+++ src/main/java/iuh/se/webecommerceserver/WebEcommerceServerApplication.java
@@ -0,0 +1,13 @@
+package iuh.se.webecommerceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEcommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebEcommerceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/se/webecommerceserver/WebEcommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.se.webecommerceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEcommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
